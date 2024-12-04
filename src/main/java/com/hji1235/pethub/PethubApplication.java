d0b46e29b0ee4784d4800f87898b5e5cc34daac5
hji1235 <hji1231@daum.net>
2024-12-04 21:09:53 +0900

    create project

---
+++ src/main/java/com/hji1235/pethub/PethubApplication.java
@@ -0,0 +1,13 @@
+package com.hji1235.pethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PethubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PethubApplication.class, args);
+	}
+
+}
